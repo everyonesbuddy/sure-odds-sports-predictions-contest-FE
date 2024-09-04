87299acccb8caf93e984875538e80785c629d126
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2024-09-04 01:40:32 -0400

    change API key for odds api

--- src/components/PicksOfTheDay.jsx
+++ src/components/PicksOfTheDay.jsx
@@ -82,7 +82,7 @@ const PicksOfTheDay = () => {
   };
 
   const getMatchUpData = async (league, gameId) => {
-    const apiKey = "7c4bb46f9759f7263e9e0c5e24f195a9";
+    const apiKey = "ecd8403d91a6f9d3601bbecad890e8a5";
     const url = `https://api.the-odds-api.com/v4/sports/${league}/odds/?apiKey=${apiKey}&regions=us&markets=h2h&oddsFormat=american&bookmakers=draftkings&eventIds=${gameId}`;
 
     try {
