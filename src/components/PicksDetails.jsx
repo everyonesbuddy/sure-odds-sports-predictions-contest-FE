5094c7621761967cd78739099e28cc34aa8add30
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2024-10-28 15:35:08 -0400

    fix eslint error for picks details page

--- src/components/PicksDetails.jsx
+++ src/components/PicksDetails.jsx
@@ -12,7 +12,7 @@ import {
 
 const PicksDetails = () => {
   const [userBets, setUserBets] = useState({});
-  const [matchupData, setMatchupData] = useState({});
+  // const [matchupData, setMatchupData] = useState({});
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
@@ -116,19 +116,19 @@ const PicksDetails = () => {
         setUserBets(aggregatedData);
 
         // Fetch matchup data for live bets
-        const liveBets = data.filter((bet) => bet.betResult === null);
-        const matchupPromises = liveBets.map((bet) =>
-          getMatchUpData(bet.league, bet.selectedGameId)
-        );
-        const matchupResults = await Promise.all(matchupPromises);
+        // const liveBets = data.filter((bet) => bet.betResult === null);
+        // const matchupPromises = liveBets.map((bet) =>
+        //   getMatchUpData(bet.league, bet.selectedGameId)
+        // );
+        // const matchupResults = await Promise.all(matchupPromises);
 
         // Create a lookup object for matchup data
-        const matchupDataObj = liveBets.reduce((acc, bet, index) => {
-          acc[bet.selectedGameId] = matchupResults[index];
-          return acc;
-        }, {});
+        // const matchupDataObj = liveBets.reduce((acc, bet, index) => {
+        //   acc[bet.selectedGameId] = matchupResults[index];
+        //   return acc;
+        // }, {});
 
-        setMatchupData(matchupDataObj);
+        // setMatchupData(matchupDataObj);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -137,38 +137,38 @@ const PicksDetails = () => {
     fetchData();
   }, []);
 
-  const getLeagueName = (leagueCode) => {
-    const leagueNames = {
-      basketball_wnba: "WNBA 🏀",
-      baseball_mlb: "MLB ⚾",
-      americanfootball_nfl: "NFL 🏈",
-      soccer_epl: "EPL ⚽",
-      icehockey_nhl: "NHL 🏒",
-      soccer_germany_bundesliga: "Bundesliga ⚽",
-      soccer_italy_serie_a: "Serie A ⚽",
-      soccer_spain_la_liga: "La Liga ⚽",
-      soccer_usa_mls: "MLS ⚽",
-    };
+  // const getLeagueName = (leagueCode) => {
+  //   const leagueNames = {
+  //     basketball_wnba: "WNBA 🏀",
+  //     baseball_mlb: "MLB ⚾",
+  //     americanfootball_nfl: "NFL 🏈",
+  //     soccer_epl: "EPL ⚽",
+  //     icehockey_nhl: "NHL 🏒",
+  //     soccer_germany_bundesliga: "Bundesliga ⚽",
+  //     soccer_italy_serie_a: "Serie A ⚽",
+  //     soccer_spain_la_liga: "La Liga ⚽",
+  //     soccer_usa_mls: "MLS ⚽",
+  //   };
 
-    return leagueNames[leagueCode] || leagueCode;
-  };
+  //   return leagueNames[leagueCode] || leagueCode;
+  // };
 
-  const getMatchUpData = async (league, gameId) => {
-    const apiKey = "402f2e4bba957e5e98c7e1a178393c8c";
-    const url = `https://api.the-odds-api.com/v4/sports/${league}/odds/?apiKey=${apiKey}&regions=us&markets=h2h&oddsFormat=american&bookmakers=draftkings&eventIds=${gameId}`;
+  // const getMatchUpData = async (league, gameId) => {
+  //   const apiKey = "402f2e4bba957e5e98c7e1a178393c8c";
+  //   const url = `https://api.the-odds-api.com/v4/sports/${league}/odds/?apiKey=${apiKey}&regions=us&markets=h2h&oddsFormat=american&bookmakers=draftkings&eventIds=${gameId}`;
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const matchup = data[0];
-      const homeTeam = matchup.home_team;
-      const awayTeam = matchup.away_team;
-      return `${homeTeam} vs ${awayTeam}`;
-    } catch (error) {
-      console.error("Error fetching matchup data:", error);
-      return "Matchup data not available";
-    }
-  };
+  //   try {
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     const matchup = data[0];
+  //     const homeTeam = matchup.home_team;
+  //     const awayTeam = matchup.away_team;
+  //     return `${homeTeam} vs ${awayTeam}`;
+  //   } catch (error) {
+  //     console.error("Error fetching matchup data:", error);
+  //     return "Matchup data not available";
+  //   }
+  // };
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
