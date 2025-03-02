c0a4f44e85654ddb40a70ea6554a9659b5a13f79
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2025-03-02 17:53:25 -0500

    second place prize change

--- src/utils/contestData.js
+++ src/utils/contestData.js
@@ -4,7 +4,7 @@ export const contest = [
     primaryImageUrl:
       "https://i.ibb.co/n8r2RKnt/Orange-and-Yellow-Illustrative-Sport-Trivia-Quiz-Presentation.png",
     firstPlacePrize: "$300 USD",
-    secondPlacePrize: "$100 USD",
+    secondPlacePrize: "$150 USD",
     thirdPlacePrize: "$50 USD",
     spreadsheetUrl:
       "https://sheet.best/api/sheets/b9c7054b-1a70-4afb-9a14-c49967e8faf8",
