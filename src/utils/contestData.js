e296b031413694d9696bcf5a4dbded50c104f010
Joseph <47672995+everyonesbuddy@users.noreply.github.com>
2025-03-04 00:54:33 -0500

    updated contest

--- src/utils/contestData.js
+++ src/utils/contestData.js
@@ -1,17 +1,17 @@
 export const contest = [
   {
-    contestName: "March Madness Pick'em",
+    contestName: "NBA Season Long Pick'em",
     primaryImageUrl:
-      "https://i.ibb.co/n8r2RKnt/Orange-and-Yellow-Illustrative-Sport-Trivia-Quiz-Presentation.png",
-    firstPlacePrize: "$300 USD",
-    secondPlacePrize: "$150 USD",
-    thirdPlacePrize: "$50 USD",
+      "https://i.ibb.co/Y4kkDNcf/Orange-and-Yellow-Illustrative-Sport-Trivia-Quiz-Presentation.png",
+    firstPlacePrize: "$1000 USD",
+    secondPlacePrize: "$350 USD",
+    thirdPlacePrize: "$150 USD",
     spreadsheetUrl:
       "https://sheet.best/api/sheets/b9c7054b-1a70-4afb-9a14-c49967e8faf8",
     sponsored: false,
     contestStartDate: "3/1/2025",
-    contestEndDate: "3/31/2025",
-    contestLeague: ["basketball_ncaab", "basketball_wncaab"],
+    contestEndDate: "6/30/2025",
+    contestLeague: ["basketball_nba"],
     availableFreePicks: 5,
   },
 ];
